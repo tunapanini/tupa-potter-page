4da1bef3f0fb55098b7d8ccf0a4c36a4801a9944
tunapanini <haneuneee@gmail.com>
2020-06-26 14:57:28 +0000

    Deploying to gh-pages from  @ 8c3e5bf5b26ed57ab6477b687f90c1e1325c9ff2 🚀

---
+++ precache-manifest.7554adb446ae05b2fe7a1a95f4ec8c58.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e5c634b08d33be22251b623223a60657",
+    "url": "https://tunapanini.github.io/tupa-potter-page/index.html"
+  },
+  {
+    "revision": "754051d4ffbe77fdb392",
+    "url": "https://tunapanini.github.io/tupa-potter-page/static/js/2.4e63f082.chunk.js"
+  },
+  {
+    "revision": "1937a0fc6634269e5f47f8181e27fdc7",
+    "url": "https://tunapanini.github.io/tupa-potter-page/static/js/2.4e63f082.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0edcd1d966b9d0fa41d0",
+    "url": "https://tunapanini.github.io/tupa-potter-page/static/js/main.529d68d3.chunk.js"
+  },
+  {
+    "revision": "f8951a52ccab616697d8",
+    "url": "https://tunapanini.github.io/tupa-potter-page/static/js/runtime-main.c66b2b75.js"
+  }
+]);
