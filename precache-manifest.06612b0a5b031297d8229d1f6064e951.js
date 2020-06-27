05db68488237494aec903eae9484a8efeaeabd48
tunapanini <haneuneee@gmail.com>
2020-06-27 06:25:55 +0000

    Deploying to gh-pages from  @ 0ab54f1a8ae9ce1c007b0f44f344b970338243a9 🚀

---
+++ precache-manifest.06612b0a5b031297d8229d1f6064e951.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "084b20f7fe52d404fe3b4faa969e085a",
+    "url": "https://tunapanini.github.io/tupa-potter-page/index.html"
+  },
+  {
+    "revision": "02239704b30c76a8de53",
+    "url": "https://tunapanini.github.io/tupa-potter-page/static/js/2.ed416bb3.chunk.js"
+  },
+  {
+    "revision": "1937a0fc6634269e5f47f8181e27fdc7",
+    "url": "https://tunapanini.github.io/tupa-potter-page/static/js/2.ed416bb3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "181e978979d372fae7f6",
+    "url": "https://tunapanini.github.io/tupa-potter-page/static/js/main.dc6eb875.chunk.js"
+  },
+  {
+    "revision": "f8951a52ccab616697d8",
+    "url": "https://tunapanini.github.io/tupa-potter-page/static/js/runtime-main.c66b2b75.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "https://tunapanini.github.io/tupa-potter-page/precache-manifest.d3cb39ecb1963bff9017531512200602.js"
+  "https://tunapanini.github.io/tupa-potter-page/precache-manifest.06612b0a5b031297d8229d1f6064e951.js"
 );
 
 self.addEventListener('message', (event) => {
